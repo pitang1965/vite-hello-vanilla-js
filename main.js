afe91801a707fa9f9fa74e16a9a8d6f4601fa180
Shoji Makino <pitang1965@gmail.com>
2021-07-29 16:26:56 +0900

    Add [Say Hello] buttons and JavaScript

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-import './style.css'
+import './style.css';
+import { getGreenting1, getGreenting2 } from './src/greetings';
+
+const button1 = document.getElementById('button1');
+const label1 = document.getElementById('label1');
+button1.addEventListener('click', () => {
+  label1.innerHTML = getGreenting1();
+});
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+  <h1>main.jsでinnnerHTMLで設定</h1>
+  <button id="button2" class="styled" type="button">Say Hello</button>
+  <label id="label2"></label>
+`;
+
+const button2 = document.getElementById('button2');
+const label2 = document.getElementById('label2');
+
+button2.addEventListener('click', () => {
+  label2.innerHTML = getGreenting2();
+});
